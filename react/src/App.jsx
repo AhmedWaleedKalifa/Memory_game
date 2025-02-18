7519c824403ace0305672e86ff925f89f659de82
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2025-02-18 17:16:43 +0200

    add some sounds in the game and hadle the fetching time

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,7 +6,7 @@ import ScoreBoard from './components/ScoreBoard';
 
 
 async function getBerryNames(number) {
-  let random=Math.floor(Math.random()*44)
+  let random = Math.floor(Math.random() * 44)
   let response = await fetch(`https://pokeapi.co/api/v2/berry/?offset=${random}&limit=20`, { mode: "cors" })
 
   let json = await response.json();
@@ -48,20 +48,24 @@ function App() {
   const [imageUrls, setImageUrls] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [selectedCards,setSelectedCards]=useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     makeImagesArray(20).then(urls => {
       setImageUrls(randomCards(urls))
+      setIsLoading(false)
     });
-
-  }, [])
+  },[])
 
   useEffect(() => {
     if (imageUrls.length > 1) {
       setImageUrls(randomCards(imageUrls));
     }
-    if(score>bestScore){
+    if (score > bestScore) {
       setBestScore(score)
+      let sound = new Audio("../../public/game-bonus-2-294436.mp3")
+      sound.play();
     }
   }, [score])
 
@@ -72,10 +76,9 @@ function App() {
       </div>
       {/* <button style={{background:"red",width:"20px",height:"20px"}} onClick={()=>{setScore(score+1)}}></button> */}
       <ScoreBoard score={score} bestScore={bestScore}></ScoreBoard>
-      {imageUrls ? (<GameBoard array={imageUrls} score={score} setScore={setScore} selectedCards={selectedCards} setSelectedCards={setSelectedCards} ></GameBoard>)
-        : (
-          <p>loading...</p>
-        )
+      {isLoading ? (<p style={{textAlign:"center",fontSize:"25px",color:"rgb(205, 39, 2)",marginTop:"70px"}}>loading...</p>)
+        :
+        (<GameBoard array={imageUrls} score={score} setScore={setScore} selectedCards={selectedCards} setSelectedCards={setSelectedCards} ></GameBoard>)
       }
     </>
 
--- react/src/components/Card.jsx
+++ react/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import '../styles/card.css'
 export default function Card({url,score,setScore,selectedCards,setSelectedCards}){
     function handleClick(){
-        console.log("url: ",url)
-        console.log("selected cards: ")
-        selectedCards.forEach(e=>{
-            console.log(e)
-        })
+        
         if(!selectedCards.includes(url)){
             // setSelectedCards(selectedCards.push(url))
+            let sound = new  Audio("../../public/mouse-click-sound-233951.mp3");  
+            sound.play();
             setScore(score+1);
             setSelectedCards([...selectedCards, url]);
         }else{
             setScore(0)
             setSelectedCards([])
+            let sound =new Audio("../../public/wronganswer-37702.mp3")
+            sound.play();
         }
     }
     return (
